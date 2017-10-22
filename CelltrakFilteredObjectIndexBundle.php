97cb9b815a0fb6d70cce7d919f45f0a7a7016031
Mike Turoff <mturoff@celltrak.com>
2017-10-22 10:35:07 -0500

    Initial commit

---
+++ CelltrakFilteredObjectIndexBundle.php
@@ -0,0 +1,15 @@
+<?php
+namespace Celltrak\FilteredObjectIndexBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+
+class CelltrakFilteredObjectIndexBundle extends Bundle
+{
+
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+    }
+}
